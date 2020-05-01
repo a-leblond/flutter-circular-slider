8ea47c3286f744a22e42587681b80e3f1d2d2e8c
Alexis Leblond <alexis.l@outlook.com>
2020-05-01 15:13:07 +0200

    Update circular_slider_paint.dart

--- lib/src/circular_slider_paint.dart
+++ lib/src/circular_slider_paint.dart
@@ -49,6 +49,7 @@ class CircularSliderPaint extends StatefulWidget {
     @required this.showHandlerOutter,
     @required this.sliderStrokeWidth,
     @required this.shouldCountLaps,
+    @required this.initialLaps
   });
 
   @override
@@ -95,6 +96,7 @@ class _CircularSliderState extends State<CircularSliderPaint> {
   void initState() {
     super.initState();
     _calculatePaintData();
+    _laps = _initialLaps();
   }
 
   // we need to update this widget both with gesture detector but
